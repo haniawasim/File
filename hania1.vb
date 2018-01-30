3c6cc1e903a14fc4dd84ff6aa457beda4669ff1e
haniawasim <35518777+haniawasim@users.noreply.github.com>
2018-01-31 00:31:14 +0500

    Update hania1.vb

--- hania1.vb
+++ hania1.vb
@@ -1,14 +1,14 @@
-﻿Module Module1
+m﻿Module Module1
 
     Sub Main()
         Dim Name As String
         Dim MemberIdNumber As String
 
         Console.WriteLine("Enter name: ")
-Name:   Console.ReadLine()
+        Name:  Console.ReadLine
 
         Console.WriteLine("Enter memberIdnumber: ")
-MemberIdNumber: Console.ReadLine()
+        MemberIdNumber: Console.ReadLine
 
         FileOpen(1, "d:/helloworld/sRec.txt", OpenMode.Append)
         FileClose(1)
