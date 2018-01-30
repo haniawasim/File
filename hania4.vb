f6f2941b99331a6cb21eba4b60c79dc183f33b5e
haniawasim <35518777+haniawasim@users.noreply.github.com>
2018-01-31 00:30:16 +0500

    Update hania4.vb

--- hania4.vb
+++ hania4.vb
@@ -4,15 +4,13 @@ Tasl 1.4
 
         Dim Name As String
         Dim MemberIdNumber As String
-        Dim NewMemberId As String
+
 
         Console.Write("Enter name: ")
-name:   Console.ReadLine()
+        name:    Console.ReadLine
         Console.WriteLine("Enter memberIdnumber: ")
-MemberIdNumber: Console.ReadLine()
-        Console.WriteLine("Enter newmemberId: ")
-newmemberId: Console.ReadLine()
-
+        MemberIdNumber:  Console.ReadLine
+        
         FileOpen(1, "d:/helloworld/sRec.txt", OpenMode.Append)
 
 
