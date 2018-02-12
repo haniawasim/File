e8ec09e09287fa154ff6f264feae328bf3488ba3
haniawasim <35518777+haniawasim@users.noreply.github.com>
2018-02-12 18:16:27 +0500

    Add files via upload

---
+++ TASK 2.2.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+Task 2.2
+    Sub Main()
+        Dim txt As String
+        Console.Write("Enter strings: ")
+        txt = Console.ReadLine
+        If Len(txt) < 2 Then
+            Console.WriteLine("")
+        Else : Console.WriteLine(Left(txt, 2) & Right(txt, 2))
+        End If
+        Console.ReadKey()
+
+    End Sub
+
+End Module
