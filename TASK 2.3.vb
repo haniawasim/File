426b399ef032deeecb919a59c7dc636f1df5f07a
haniawasim <35518777+haniawasim@users.noreply.github.com>
2018-02-12 18:22:52 +0500

    Add files via upload

---
+++ TASK 2.3.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+Task 2.3
+
+    Sub Main()
+        Dim s As String
+        Dim first As Char = "*"
+        Dim symb As String
+        Dim newS As String
+        Console.Write("Enter string: ")
+        s = Console.ReadLine()
+        first = s(0)
+        newS = first
+        For i = 1 To Len(s) - 1
+            If s(i) <> first Then
+                newS = newS & s(i)
+            Else : newS = newS & symb
+
+            End If
+        Next
+        Console.WriteLine(newS)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
